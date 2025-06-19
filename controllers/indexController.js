7a366fe7c940b50e2177debda9cd0548a656056b
SaraToth <saratoth101@gmail.com>
2025-06-19 18:26:36 +0900

    Feat: Update index to search db and display result

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,9 +1,53 @@
 const db = require("../db/queries");
 
 const getIndex = async (req, res) => {
+    const searchQuery = req.query.search;
+
+        // CHECK that the searchTerm is defined
+        // TRY searching for any username that INCLUDES the searchTerm in our db
+        // NO RESULT -> Alert user that user is not in the db
+        // RESULT -> Display a list of matching users
+
+
     try{
         const usernames = await db.getAllUsernames();
-        res.render("index", { usernames: usernames });
+
+        if (searchQuery) {
+            try {
+                const searchResults = await db.searchUsernames(searchQuery);
+                if (searchResults.length > 0) {
+                    res.render("index", 
+                        { 
+                            usernames: usernames, 
+                            searchResults: searchResults, 
+                            noMatches: false,
+                            searchQuery: searchQuery || "",
+                        });
+                    return;
+                } else {
+                    res.render("index", 
+                        { 
+                            usernames: usernames, 
+                            searchResults: [], 
+                            noMatches: true,
+                            searchQuery: searchQuery || "",
+                        });
+                    return;
+                }
+
+            } catch (error) {
+                console.error("Error searching for usernames:", error);
+                res.status(500).send("Internal Server Error");
+            }
+        }
+
+        res.render("index", 
+            { 
+                usernames: usernames, 
+                searchResults: [], 
+                noMatches: true,
+                searchQuery: searchQuery || "", 
+            });
     } catch (error) {
         console.error("Error retrieving usernames:", error);
         res.status(500).send("Internal Server Error");
