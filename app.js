fe8869d22f64988a48b9e9e30a390da67aec77f6
SaraToth <saratoth101@gmail.com>
2025-06-19 16:23:30 +0900

    Feat: 404 error handler for non-existing routes

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ app.set("view engine", "ejs");
 
 app.use("/", indexRouter);
 
+app.use((req, res, next) => {
+    res.status(404).send("404: Page does not exist");
+});
+
 app.use((err, req, res, next) => {
     console.error(err);
     res.status(500).send(err);
