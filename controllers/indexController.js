58936c9b7e5938e711ffb9f82ffe729c1bc0e925
SaraToth <saratoth101@gmail.com>
2025-06-19 16:19:17 +0900

    Fix: When !usernames index should NOT be (404)

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,12 +3,7 @@ const db = require("../db/queries");
 const getIndex = async (req, res) => {
     try{
         const usernames = await db.getAllUsernames();
-        if (!usernames) {
-            res.status(404).send("No usernames can be found"); // switch to custom 404
-            return;
-        }
-        const usernameList = usernames.map(user => user.username).join(", ");
-        res.render("index", { users: usernameList });
+        res.render("index", { usernames: usernames });
     } catch (error) {
         console.error("Error retrieving usernames:", error);
         res.status(500).send("Internal Server Error");
