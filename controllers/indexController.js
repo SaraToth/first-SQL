384246b0473e67d9d76fc7eabc280b1391935521
SaraToth <saratoth101@gmail.com>
2025-06-19 18:52:01 +0900

    Refactor: async fn errors to use asyncHandler

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -1,73 +1,50 @@
 const db = require("../db/queries");
+const asyncHandler = require("express-async-handler");
 
-const getIndex = async (req, res) => {
+const getIndex = asyncHandler(async (req, res) => {
     const searchQuery = req.query.search;
+    const usernames = await db.getAllUsernames();
 
-        // CHECK that the searchTerm is defined
-        // TRY searching for any username that INCLUDES the searchTerm in our db
-        // NO RESULT -> Alert user that user is not in the db
-        // RESULT -> Display a list of matching users
-
-
-    try{
-        const usernames = await db.getAllUsernames();
-
-        if (searchQuery) {
-            try {
-                const searchResults = await db.searchUsernames(searchQuery);
-                if (searchResults.length > 0) {
-                    res.render("index", 
-                        { 
-                            usernames: usernames, 
-                            searchResults: searchResults, 
-                            noMatches: false,
-                            searchQuery: searchQuery || "",
-                        });
-                    return;
-                } else {
-                    res.render("index", 
-                        { 
-                            usernames: usernames, 
-                            searchResults: [], 
-                            noMatches: true,
-                            searchQuery: searchQuery || "",
-                        });
-                    return;
-                }
-
-            } catch (error) {
-                console.error("Error searching for usernames:", error);
-                res.status(500).send("Internal Server Error");
-            }
+    if (searchQuery) {
+        const searchResults = await db.searchUsernames(searchQuery);
+        if (searchResults.length > 0) {
+            res.render("index", 
+                { 
+                    usernames: usernames, 
+                    searchResults: searchResults, 
+                    noMatches: false,
+                    searchQuery: searchQuery || "",
+                });
+            return;
+        } else {
+            res.render("index", 
+                { 
+                    usernames: usernames, 
+                    searchResults: [], 
+                    noMatches: true,
+                    searchQuery: searchQuery || "",
+                });
+            return;
         }
-
-        res.render("index", 
-            { 
-                usernames: usernames, 
-                searchResults: [], 
-                noMatches: true,
-                searchQuery: searchQuery || "", 
-            });
-    } catch (error) {
-        console.error("Error retrieving usernames:", error);
-        res.status(500).send("Internal Server Error");
     }
 
-};
+    res.render("index", 
+        { 
+            usernames: usernames, 
+            searchResults: [], 
+            noMatches: true,
+            searchQuery: searchQuery || "", 
+        });
+});
 
 const getNew = (req, res) => {
     res.render("newUser");
 };
 
-const postNew = async (req, res) => {
-    try {
-        const { username } = req.body;
-        await db.insertUsername(username);
-        res.redirect("/");
-    } catch (error) {
-        console.error("Error posting new username:", error);
-        res.status(500).send("Internal Server Error");
-    }
-};
+const postNew = asyncHandler(async (req, res) => {
+    const { username } = req.body;
+    await db.insertUsername(username);
+    res.redirect("/");
+});
 
 module.exports = { getIndex, getNew, postNew };
